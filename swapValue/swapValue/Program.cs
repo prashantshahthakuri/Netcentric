306435ab61417b363e405a1ed295ee45f1f291bf
prashantshahthakuri <thakuriprashant93@gmail.com>
2024-11-25 09:23:47 +0545

    1st assignment

---
+++ swapValue/swapValue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
